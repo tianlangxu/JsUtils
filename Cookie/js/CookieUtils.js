4e08ac7739122806bcb9d9af8447ec3c219c80fe
QFLaoXu <854560715@qq.com>
2017-11-01 20:49:33 +0800

    增加Cookie封装库

---
+++ Cookie/js/CookieUtils.js
@@ -0,0 +1,98 @@
+/**
+ * Coock Utils
+ */
+(function(document, owner) {
+
+	/**
+	 * 需求
+	 * 1、存储Cookie值
+	 * 2、获取指定Cookie值
+	 * 3、模糊查询cookie值
+	 */
+
+	/**
+	 * 设置Cookie信息
+	 * @param {String} _key
+	 * @param {String} _value
+	 * @param {Numer} _expires
+	 * @param {String} _path
+	 */
+	owner.setCookie = function(_key, _value, _expires, _path) {
+		_key = _key || "";
+		_value = _value || "";
+		var result = "";
+
+		if(!_key) {
+			return;
+		}
+
+		var jsonObj = {
+			"value": _value
+		};
+		result = _key + "=" + encodeURIComponent(JSON.stringify(jsonObj)) + ";"
+
+		// 获取过期时间
+		if(typeof(_expires) == "number") {
+			// 表示设置为expires的值
+			// 设置过期时间
+			var d = new Date();
+			d.setDate(d.getDate() + _expires);
+			result += "expires=" + d + ";" + "path=" + _path;
+		} else if(typeof(_expires) == "string") {
+			result += "path=" + _expires;
+		}
+		document.cookie = result;
+	}
+
+	/**
+	 * 获取指定名称的Cookie值
+	 * @param {Object} _name
+	 */
+	owner.getCookie = function(_name) {
+		var info = document.cookie;
+		var cookieArr = info.split("; ");
+
+		for(var i = 0, l = cookieArr.length; i < l; i++) {
+			var valueArr = cookieArr[i].split("=");
+			if(valueArr[0] == _name) {
+				return JSON.parse(decodeURIComponent(valueArr[1])).value;
+
+			}
+		}
+	}
+	
+	/**
+	 * 获取开头为指定名称的Cookie信息
+	 * @param {Object} _name
+	 * @param {Object} callback
+	 */
+	owner.getCookies = function(_name, callback) {
+		var info = document.cookie;
+		var cookieArr = info.split("; ");
+		var getInfoArr = [];
+
+		for(var i = 0, l = cookieArr.length; i < l; i++) {
+			var valueArr = cookieArr[i].split("=");
+
+			var key = valueArr[0];
+			if(eval("/^" + _name + "/").test(key)) {
+				var value = JSON.parse(decodeURIComponent(valueArr[1])).value;
+				getInfoArr.push([key, value]);
+
+				if(callback) {
+					callback(key, value);
+				}
+			}
+		}
+		return getInfoArr;
+	}
+	
+	/**
+	 * 移除指定Cookie信息 
+	 * @param {Object} _name
+	 */
+	owner.removeCookie = function (_name){
+		this.setCookie(_name,"",-1);
+	}
+
+})(document, window.CookieUtils = {});
